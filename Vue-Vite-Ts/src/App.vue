4edb10cbefc47ef8797e63d2a1989d5796bdbe70
zhuba-Ahhh <3477826311@qq.com>
2024-03-14 22:16:06 +0800

    feat: Vue-Vite-Ts

---
+++ Vue-Vite-Ts/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="text-center selection:bg-green-100">
+    <img class="mx-auto mt-12 mb-8" alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Vite + Vue 3 + Typescript + Tailwindcss v3" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
---
+++ Vue-Vite-Ts/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1
+    class="bg-gradient-to-r from-green-600 to-sky-400 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
+    {{ msg }}
+  </h1>
+  <p class="mt-4 space-x-4">
+    <a href="https://vitejs.dev/guide/features.html"
+      class="text-emerald-500 underline transition-all hover:text-emerald-400" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" class="text-emerald-500 underline transition-all hover:text-emerald-400"
+      target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" class="my-6 rounded bg-gray-300 px-4 py-2 transition-all hover:bg-gray-200" @click="count++">
+    count is: {{ count }}
+  </button>
+  <p>
+    Edit
+    <code class="text-gray-400">components/HelloWorld.vue</code> to test hot
+    module replacement.
+  </p>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
