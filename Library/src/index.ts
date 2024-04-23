643ca50937924aa28552200f4dd4978df03a2047
zhuba-Ahhh <3477826311@qq.com>
2024-04-23 17:11:29 +0800

    fix: :bug: json.parse

--- Library/src/index.ts
+++ Library/src/index.ts
@@ -1,10 +1,18 @@
 export default 'Hello father 4!';
 
+const safeParse = (str: string) => {
+  try {
+    return JSON.parse(str);
+  } catch (e) {
+    return str;
+  }
+};
+
 export let versionLog = () => {
   if (process.env.NODE_ENV === 'production') {
     console.log(
       'background: #FA6400;color: #fff;padding: 2px 6px;border-radius: 4px;',
-      `${JSON.parse(VERSION)}`,
+      `${safeParse(VERSION)}`,
     );
     versionLog = () => {};
   }
