c1317ef95b7e2b9e13b417cc910061dfcde58484
zhuba-Ahhh <3477826311@qq.com>
2024-04-26 15:05:36 +0800

    feat: :sparkles: add Library demo

---
+++ Library/src/Resizable/index.ts
@@ -0,0 +1,4 @@
+import { Resizable } from './Resizable';
+export { Resizable } from './Resizable';
+export type { ResizableProps } from './Resizable';
+export default Resizable;
--- React-Vite-Ts/src/App.tsx
+++ React-Vite-Ts/src/App.tsx
@@ -2,13 +2,48 @@ import { Button } from 'antd';
 import FaviconSVG from 'assets/favicon.svg';
 import { uuid } from './utils';
 import css from './index.module.less';
+import { Resizable } from '../../Library/dist/esm';
+import { useCallback, useState } from 'react';
 
 function App() {
+  const [style, setStyle] = useState<React.CSSProperties>({
+    width: 100,
+    height: 100,
+    backgroundColor: '#ff4d4f',
+  });
+
+  const onResize = useCallback(
+    ({ width, height }: Pick<React.CSSProperties, 'height' | 'width'>) => {
+      setStyle((pre) => ({ ...pre, width, height }));
+    },
+    [style]
+  );
   return (
     <div className={`flex items-center justify-center ${css.test}`}>
-      <img src={FaviconSVG} className="logo" alt="Vite logo" height={100} width={100} key={uuid()} />
-      <Button onClick={() => console.log('Hello')} key={uuid()}>Hello</Button>
+      <img
+        src={FaviconSVG}
+        className="logo"
+        alt="Vite logo"
+        height={100}
+        width={100}
+        key={uuid()}
+      />
+      <Button onClick={() => console.log('Hello')} key={uuid()}>
+        Hello
+      </Button>
       {uuid()}
+      <Resizable
+        onResize={onResize}
+        axis={'both'}
+        onResizeStart={() => {
+          console.log('start');
+        }}
+        onResizeStop={() => {
+          console.log('stop');
+        }}
+      >
+        <div style={style} />
+      </Resizable>
     </div>
   );
 }
