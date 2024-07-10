dfb3480dbc5e7d8d80c7b3b558ade0be4dfccbd7
zhuba-Ahhh <3477826311@qq.com>
2024-07-10 21:49:17 +0800

    refactor: :recycle: remove useless

--- template/React-Vite-Ts/src/App.tsx
+++ template/React-Vite-Ts/src/App.tsx
@@ -3,24 +3,9 @@ import FaviconSVG from 'assets/favicon.svg';
 import { uuid } from './utils';
 import { uuid1 } from 'zhuba-tools';
 import css from './index.module.less';
-import { Resizable } from '../../Library/dist/esm';
-import { useCallback, useState } from 'react';
 import { useNumStore } from 'store';
 
 function App() {
-  const [style, setStyle] = useState<React.CSSProperties>({
-    width: 100,
-    height: 100,
-    backgroundColor: '#ff4d4f',
-  });
-
-  const onResize = useCallback(
-    ({ width, height }: Pick<React.CSSProperties, 'height' | 'width'>) => {
-      setStyle((pre) => ({ ...pre, width, height }));
-    },
-    []
-  );
-
   const { num, increasePopulation } = useNumStore();
   return (
     <>
@@ -31,30 +16,17 @@ function App() {
           alt="Vite logo"
           height={100}
           width={100}
-          // key={uuid()}
+          key={uuid()}
         />
         <Button
           onClick={() => {
             console.log('Hello');
             increasePopulation();
           }}
-          // key={uuid()}
+          key={uuid1()}
         >
           {num}
         </Button>
-        <Resizable
-          onResize={onResize}
-          axis={'both'}
-          onResizeStart={() => {
-            console.log('start');
-          }}
-          onResizeStop={() => {
-            console.log('stop');
-          }}
-          key={uuid1()}
-        >
-          <div style={style} />
-        </Resizable>
       </div>
       <div className={`flex items-center justify-center`}>
         <Image src={'https://api.52starxi.cn/api/img-4k/img.php'} style={{ width: '80vw' }}></Image>
